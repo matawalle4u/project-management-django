39e9b559cd6e217fff5324d9a0e9c8cb86cd7517
Adam Mustapha <matawalle4u@gmail.com>
2023-09-30 18:00:46 +0100

    implemented setting an admin role to a user

--- register/views.py
+++ register/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User, Group
 
 from .forms import RegisterForm
 
@@ -40,4 +42,29 @@ def logout_view(request):
     # Redirect to a success page.
             
 
-    
+# Define a custom user check function to restrict admin role assignment to certain users (e.g., superusers).
+def is_superuser(user):
+    return user.is_superuser
+
+@login_required
+@user_passes_test(is_superuser)  # Use the custom user check function.
+def toggle_admin_role(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+
+        # Check if the user is currently an admin.
+        if user.groups.filter(name='Admin').exists():
+            # Remove the user from the 'Admin' group.
+            admin_group = Group.objects.get(name='Admin')
+            user.groups.remove(admin_group)
+            messages.success(request, f'{user.username} is no longer an admin.')
+        else:
+            # Add the user to the 'Admin' group.
+            admin_group = Group.objects.get(name='Admin')
+            user.groups.add(admin_group)
+            messages.success(request, f'{user.username} is now an admin.')
+
+        return redirect('admin_dashboard')  # Redirect to an admin dashboard or user list page.
+    except User.DoesNotExist:
+        messages.error(request, 'User not found.')
+        return redirect('admin_dashboard')  # Redirect to an admin dashboard or user list page.
