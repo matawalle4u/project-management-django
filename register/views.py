81b1d62e0d8345ff16d1f9a4a9ab4a0527195e22
Adam Mustapha <matawalle4u@gmail.com>
2023-10-01 01:10:15 +0100

    implemented editing user and showing a user

--- register/views.py
+++ register/views.py
@@ -14,11 +14,47 @@ def register(response):
         form = RegisterForm(response.POST)
         if form.is_valid():
             form.save()
-        return redirect("/")
+        return redirect("/projects")
     else:
         form = RegisterForm()
 
     return render(response, "register/register.html", {"form":form})
+
+@login_required
+def get_users(response):
+    #all_users = User.objects.filter(id!=response.user.id)
+    all_users = User.objects.all()
+    if response.user.is_superuser:
+        return render(response, "accounts/admin_home.html", {"all_users":all_users})
+    else:
+        return redirect('/admin-user/login')
+
+@login_required
+def show_user(request, pk):
+    user = User.objects.get(pk=pk)
+    return render(request, "register/user_show.html", {'user':user})
+
+@login_required
+def edit_user(request, pk):
+    user = User.objects.get(pk=pk)
+
+    
+    if request.method == 'POST':
+        form = RegisterForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('/admin-user')
+    else:
+        form = RegisterForm(instance=user)
+    return render(request, 'register/register.html', {'form': form, 'user': user})
+
+
+    #return render(request, "register/register.html", {'user':user})
+
+@login_required
+def delete_user(request, pk):
+    user = User.objects.get(pk=pk)
+    return render(request, "register/user_show.html", {'user':user})
 
 def login_view(request):
     if request.method == 'POST':
@@ -29,6 +65,21 @@ def login_view(request):
             login(request, form.get_user())
             # Redirect to a success page, e.g., the user's profile page
             return redirect('/projects')
+    else:
+        # Display an empty AuthenticationForm for GET requests
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
+
+def admin_login(request):
+    if request.method == 'POST':
+        # Create an instance of the AuthenticationForm with POST data
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # Log in the user if the form is valid
+            login(request, form.get_user())
+            # Redirect to a success page, e.g., the user's profile page
+            return redirect('/admin-user')
     else:
         # Display an empty AuthenticationForm for GET requests
         form = AuthenticationForm()
