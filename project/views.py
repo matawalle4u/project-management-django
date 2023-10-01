7e9485cb9200d171d56c1861829f41d38cbfa777
Adam Mustapha <matawalle4u@gmail.com>
2023-10-01 01:14:56 +0100

    Done

--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Project
+from django.contrib.auth.models import User, Group
 
 from .forms import ProjectForm
 
@@ -22,28 +23,24 @@ def new(response):
     return render(response, "project/new.html", {"form":form})
 
 @login_required
-def get_user_project(request):
-    
-    user_projects = Project.objects.filter(owner=request.user)
-    if request.method == "POST":
-        form = ProjectForm(request.POST)
-        if form.is_valid():
-            saving = form.save(commit=False)
-            saving.owner = request.user
-            
-            saving.save()
-        return redirect("/project/new")
-    else:
-        form = ProjectForm()
+def show_project(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, "project/project_show.html", {'project':project})
 
-    return render(request, "project/project_lists.html", {'user_projects':user_projects})
+@login_required
+def get_user_project(request):
+
+    user_projects = Project.objects.filter(owner=request.user)
+    return render(request, "project/project_lists.html", {'user_projects':user_projects, 'user':request.user})
 
     
 
 @login_required
 def projects(request):
     all_projects = Project.objects.all()
-    return render(request, 'project/new.html', {'all_projects': all_projects})
+    # Check if the user is currently an admin.
+    
+    return render(request, 'project/project_lists.html', {'all_projects': all_projects})
 
 
 @login_required
@@ -53,7 +50,7 @@ def edit_project(request, pk):
         form = ProjectForm(request.POST, instance=project)
         if form.is_valid():
             form.save()
-            return redirect('/project/new')
+            return redirect('/projects')
     else:
         form = ProjectForm(instance=project)
     return render(request, 'project/new.html', {'form': form, 'task': project})
@@ -62,4 +59,4 @@ def edit_project(request, pk):
 def delete_project(request, pk):
     project = Project.objects.get(pk=pk)
     project.delete()
-    return redirect('/project/new')
+    return redirect('/projects')
